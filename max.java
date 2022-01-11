95d80ed66dc89812cc9002f3f9dece07df2471c5
nishen99 <97047087+nishen99@users.noreply.github.com>
2022-01-11 08:53:36 +0530

    Create max.java
    find max value on java programe

---
+++ max.java
@@ -0,0 +1,21 @@
+class max{
+
+public static void main(String args[ ])
+
+{
+
+int num1=10;
+int num2=30;
+
+if (num1>num2){
+System.out.println("num1 greterthan num2");
+}
+
+else{
+System.out.println("num2 gretherthan num1");
+}
+
+
+
+}
+}
